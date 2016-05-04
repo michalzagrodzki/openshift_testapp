dd4eb4d3f48b0d34d0ab2503b2d7b3ff4d51fef8
Michal Zagrodzki <michalzagrodzki@gmail.com>
2016-05-04 14:52:30 +0200

    Change content types

--- utils/content-types.js
+++ utils/content-types.js
@@ -1,8 +1,11 @@
 module.exports = {
-  'html' : 'text/html',
-  'css'  : 'text/css',
-  'tag'  : 'text/html',
-  'ico'  : 'image/x-icon',
-  'png'  : 'image/png',
-  'svg'  : 'image/svg+xml'
+  'txt': 'text/plain',
+  'html': 'text/html',
+  'css': 'text/css',
+  'js': 'application/javascript',
+  'jpg': 'image/jpeg',
+  'jpeg': 'image/jpeg',
+  'gif': 'image/gif',
+  'png': 'image/png',
+  'svg': 'image/svg+xml'
 }
