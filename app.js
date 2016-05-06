2454249426aed1556446c7f82c26c5c0f51b5a3f
Michal Zagrodzki <michalzagrodzki@gmail.com>
2016-05-06 15:29:37 +0200

    Correct conditionals in app.js

--- app.js
+++ app.js
@@ -13,26 +13,12 @@ let server = http.createServer(function (req, res) {
   // IMPORTANT: Your application HAS to respond to GET /health with status 200
   //            for OpenShift health monitoring
 
-  if (url == '/health') {
+if (url == '/health') {
     res.writeHead(200);
     res.end();
   } else if (url.indexOf('/info/') == 0) {
     res.setHeader('Content-Type', 'application/json');
     res.setHeader('Cache-Control', 'no-cache, no-store');
-  } else if {
-    fs.readFile('./static' + url, function (err, data) {
-      if (err) {
-        res.writeHead(404);
-        res.end();
-      } else {
-        let ext = path.extname(url).slice(1);
-        res.setHeader('Content-Type', contentTypes[ext]);
-        if (ext === 'html') {
-          res.setHeader('Cache-Control', 'no-cache, no-store');
-        }
-        res.end(data);
-      }
-    });
   } else {
     fs.readFile('./dist' + url, function (err, data) {
       if (err) {
@@ -47,8 +33,9 @@ let server = http.createServer(function (req, res) {
         res.end(data);
       }
     });
-}
+  }
 });
+
 
 server.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', function () {
   console.log(`Application worker ${process.pid} started...`);
